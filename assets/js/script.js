26afcad48948c59c4b3b3ad3fe20af4742ca2673
jackfeely <jack.feely@gmail.com>
2017-01-13 14:15:06 -0500

    implement styled template

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){  
+  $(".button-collapse").sideNav({
+    menuWidth: 200,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true
+  });
+
+  var colors = {
+    'about': 'rgb(142,85,114)',
+    'current_projects':  '#F46036',
+    'current': '#F46036',
+    'finished_projects': '#2E294E',
+    'finished': '#2E294E'
+  }
+  var address = window.location.href.split('/'),
+      pagename = address[address.length - 1],
+      navtag = pagename.split('_')[0];
+
+  $('body').css('background-color', colors[pagename]);
+  $('.card-action a').css('color', colors[pagename]);
+  $('nav a').attr('style', 'color: ' + colors[pagename] + ' !important;');
+  $("a:contains('" + navtag + "')").parent().css('background-color', colors[pagename]);
+  $("a:contains('" + navtag + "')").css('color', 'white');
+});
